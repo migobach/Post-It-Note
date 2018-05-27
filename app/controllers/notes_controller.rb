7bd44bd0f857b5756732ce424d48f55ed316f574
Michael Bach <migobach@gmail.com>
2018-05-27 13:00:39 -0600

    add delete

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -12,12 +12,30 @@ class NotesController < ApplicationController
   end
 
   def create 
-    @note = Note.new(note_params[:id])
+    @note = Note.new(note_params)
     if @note.save 
-      redirect_to note_path
+      redirect_to notes_path
     else
       render :new
     end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+  
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to notes_path 
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
   end
 
 
